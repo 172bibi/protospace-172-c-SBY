916963e4ae8e3225a7809e09c7ad52c9c1c440ea
SeiraHayakawa <peterseira0824@gmail.com>
2024-05-23 16:21:40 +0900

    編集完了

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[7.0].define(version: 2024_05_23_054056) do
-  
+ActiveRecord::Schema[7.0].define(version: 2024_05_23_033304) do
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -42,7 +40,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_05_23_054056) do
   end
 
   create_table "prototypes", charset: "utf8", force: :cascade do |t|
-
     t.string "title", null: false
     t.text "catch_copy", null: false
     t.text "concept", null: false
@@ -50,7 +47,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_05_23_054056) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_prototypes_on_user_id"
-
   end
 
   create_table "users", charset: "utf8", force: :cascade do |t|
@@ -71,7 +67,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_05_23_054056) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-
   add_foreign_key "prototypes", "users"
-
 end
