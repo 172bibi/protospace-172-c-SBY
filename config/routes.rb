836c4faeaf3bece3929c971084e4b8f12ef7ca89
SeiraHayakawa <peterseira0824@gmail.com>
2024-05-23 19:24:33 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: "prototypes#index"
 
 
-  resources :prototypes, only: [:index, :new, :create, :edit, :update, :show]
+  resources :prototypes
   resources :users
 
 
