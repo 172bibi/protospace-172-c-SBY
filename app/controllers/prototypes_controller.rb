3342e348bcfb7c0930344d85c6c7cc041a1b0a3d
172bibi <g84.h.g84ib@gmail.com>
2024-05-24 11:58:47 +0900

    ログアウト状態のページの遷移

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,5 @@
 class PrototypesController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :edit, :destroy]
   before_action :correct_user, only: [:edit, :update]
   before_action :logged_in_user, only: [:edit, :update]
 
